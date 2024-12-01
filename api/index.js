b10187aa321f522e4d5b08309b72ea7c1512aef1
Pr1M3zA <priscilav.mezal@gmail.com>
2024-12-01 14:30:51 -0700

    Added JSONWebToken and BCrypt

--- api/index.js
+++ api/index.js
@@ -1,37 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const connect = require('./db.js');
 const app = express();
+const bcrypt = require('bcrypt');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 app.set('port', process.env.PORT);
 
-app.get('/users' , async (req, res) => {
-    let db;
-    try {
-        db = await connect();
-        const query = "SELECT * FROM users";
-        const [rows] = await db.execute(query);
-        res.json({
-            data: rows,
-            status: 200
-        });
-    }catch(err) {
-        return res.status(500).json({message: db ? err.sqlMessage : "DB connection"}) 
-        throw err;
-    }finally {
-        if (db)  await db.end(); // Cerrar la conexión
-    }    
-});
-
-app.get('/users/:id', async (req, res) => { 
+app.get('/user', checkToken, async (req, res) => { 
     let db;
     try{
-        const id = req.params.id;
         db = await connect();
-        const query = `SELECT * FROM users WHERE id=${id}`;
+        const query = `SELECT * FROM users WHERE id=${req.idUser}`;
         const [rows] = await db.execute(query);
         res.json({
             data: rows,
@@ -45,26 +28,31 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
-app.get('/login/:email&:pass', async (req, res) => { 
+app.post('/login', async (req, res) => { 
     let db;
     try{
-        const { email, pass } = req.params
+        const { email, pass } = req.body
         db = await connect();
-        const query = `SELECT id, pass, SHA1('${pass}') AS reqpass FROM users WHERE email ='${email}'`;
+        const query = `SELECT id, pass FROM users WHERE email ='${email}'`;
         
         const [rows] = await db.execute(query);
         let idUser = 0;
         if(rows.length > 0){
-            if(rows[0].pass.localeCompare(rows[0].reqpass) == 0)
+            if( bcrypt.compareSync(pass, rows[0].pass))
                 idUser=rows[0].id
             else
                 throw new Error('Contraseña incorrecta');
         }
         else
             throw new Error('El usuario no existe');
+        const token = jwt.sign({
+            idUser, 
+            exp: Date.now() + 60 * 10000
+        }, process.env.SECRET)
+        console.log(token);
         res.json({
-            data: idUser,
-            status: 200
+        data: token,
+        status: 200
         })
     } catch(err) {
         return res.status(500).json({message: err.message}) 
@@ -74,13 +62,33 @@ app.get('/login/:email&:pass', async (req, res) => {
     }
 });
 
+app.get('/cardTypes' , async (req, res) => {
+    let db;
+    try {
+        db = await connect();
+        const query = "SELECT * FROM cardTypes";
+        const [rows] = await db.execute(query);
+        res.json({
+            data: rows,
+            status: 200
+        });
+    }catch(err) {
+        return res.status(500).json({message: db ? err.sqlMessage : "DB connection"}) 
+        throw err;
+    }finally {
+        if (db)  await db.end();
+    }    
+});
+
+
 app.post('/users', async (req, res) => { 
     let db;
+    const saltRounds = 10;
     try{
         const { first_name, last_name, email, pass } = req.body
-        let idUser;
         db = await connect();
-        const query = `CALL SP_CREATE_USER('${first_name}', '${last_name}', '${email}', '${pass}')`;
+        const hashPassword = bcrypt.hashSync(pass, saltRounds);
+        const query = `CALL SP_CREATE_USER('${first_name}', '${last_name}', '${email}', '${hashPassword}')`;
         const [rows] = await db.execute(query);
         res.json({
             data: rows,
@@ -94,9 +102,42 @@ app.post('/users', async (req, res) => {
     }
 });
 
+app.post('/cards', async (req, res) => { 
+    let db;
+    try{
+        const { user_id, card_number, card_type, expiration_date, CVV, balance } = req.body
+        db = await connect();
+        const query = `CALL SP_NEW_CARD(${user_id}, '${card_number}', ${card_type}, '${expiration_date}', '${CVV}')`;
+        const [rows] = await db.execute(query);
+        res.json({
+            data: rows,
+            status: 200
+        })
+    } catch(err) {
+       return res.status(500).json({message: db ? err.sqlMessage : "DB connection"}) 
+    } finally {
+        if(db)
+            db.end();
+    }
+});
 
+function checkToken(req, res, next){
+    const token = req.headers['authorization'];
+    if(typeof token === 'undefined')
+        res.sendStatus(403)
+    else{
+        const tokenData = jwt.verify(token, process.env.SECRET, (err, data) =>{
+            if(err)
+                res.sendStatus(403);
+            else
+                req.idUser=data.idUser
+        })
+        next();
+    }
+}
 
 app.listen(app.get('port'), () =>{
     console.log('app escuchando en el puerto', app.get('port'))
 });
+
 
