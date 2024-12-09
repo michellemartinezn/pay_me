b0a8e4caba6d09cab916e5c8ef54207243e380a5
michellem <michellem_21@alu.uabcs.mx>
2024-12-08 22:47:51 -0700

    Added log Out confirmation alert

--- logout.js
+++ logout.js
@@ -2,9 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const logoutButton = document.getElementById('logout');
     if (logoutButton) {
         logoutButton.addEventListener('click', () => {
-            sessionStorage.removeItem("token");  
-            window.location.href = '../home_page/home_page.html';
+            Swal.fire({
+                title: "Confirmar",
+                text: "¿Estás seguro que quieres cerrar sesión?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Confirmar",
+                cancelButtonText: "Cancelar"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    sessionStorage.removeItem("token");
+                    window.location.href = '../home_page/home_page.html';
+                }
+            });
         });
     }
 });
+
+
 
